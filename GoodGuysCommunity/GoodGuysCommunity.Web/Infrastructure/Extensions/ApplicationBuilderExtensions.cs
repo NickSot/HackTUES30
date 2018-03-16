2a7e92413b310e723c4ed71e0c783c3b0a2ee0df
titi2001 <titi_2001@abv.bg>
2018-03-16 19:05:41 +0200

    Resources Design Updated

--- GoodGuysCommunity/GoodGuysCommunity.Web/Infrastructure/Extensions/ApplicationBuilderExtensions.cs
+++ GoodGuysCommunity/GoodGuysCommunity.Web/Infrastructure/Extensions/ApplicationBuilderExtensions.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
 
 namespace GoodGuysCommunity.Web.Infrastructure.Extensions
 {
