608871ce6ae2819e5ada13c2a550b97ca34fdfcf
unknown <nikola.s.sotirov@gmail.com>
2018-03-16 10:51:59 +0200

    Added forum post

--- GoodGuysCommunity/GoodGuysCommunity.Services/Interfaces/IPostService.cs
+++ GoodGuysCommunity/GoodGuysCommunity.Services/Interfaces/IPostService.cs
@@ -9,5 +9,7 @@ namespace GoodGuysCommunity.Services.Interfaces
     public interface IPostService
     {
         IQueryable<Post> GetAll();
+        void Add(string Name, string Content, string AuthorId);
+        void Update();
     }
 }
--- GoodGuysCommunity/GoodGuysCommunity.Web/Areas/Forum/Models/PostListViewModel.cs
+++ GoodGuysCommunity/GoodGuysCommunity.Web/Areas/Forum/Models/PostListViewModel.cs
@@ -12,5 +12,8 @@ namespace GoodGuysCommunity.Web.Areas.Forum.Models
 
         public string Name { get; set; }
 
+        public string Content { get; set; }
+
+        public DateTime SubmitDate { get; set; }
     }
 }
