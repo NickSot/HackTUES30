b90cc0c54c664ce672e1e533b359d0169bef3477
GenchoBG <genchobg11@gmail.com>
2018-03-16 11:15:21 +0200

    Fixed non-compiling files after merge

--- GoodGuysCommunity/GoodGuysCommunity.Services/PostService.cs
+++ GoodGuysCommunity/GoodGuysCommunity.Services/PostService.cs
@@ -1,9 +1,7 @@
 ﻿using GoodGuysCommunity.Data;
 using GoodGuysCommunity.Services.Interfaces;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using GoodGuysCommunity.Data.Models;
 
 
 namespace GoodGuysCommunity.Services
