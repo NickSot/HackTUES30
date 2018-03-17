a86dcaca56211223ede3a5125c71c081d523d8f9
unknown <nikola.s.sotirov@gmail.com>
2018-03-17 22:26:58 +0200

    Adding resource display on main page

--- GoodGuysCommunity/GoodGuysCommunity.Web/Models/ManageViewModels/PostsListAndResourcesViewModel.cs
+++ GoodGuysCommunity/GoodGuysCommunity.Web/Models/ManageViewModels/PostsListAndResourcesViewModel.cs
@@ -6,8 +6,9 @@ using System.Threading.Tasks;
 
 namespace GoodGuysCommunity.Web.Models.ManageViewModels
 {
-    public class PostsListViewModel
+    public class PostsListAndResourcesViewModel
     {
         public List<Post> posts { get; set; }
+        public List<Resource> resources { get; set; }
     }
 }
