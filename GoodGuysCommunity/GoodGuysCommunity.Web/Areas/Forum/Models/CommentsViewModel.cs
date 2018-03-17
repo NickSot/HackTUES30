d4a93b599c114711ee4618f419b60ccfac65b0c5
GenchoBG <genchobg11@gmail.com>
2018-03-17 11:01:10 +0200

    Merge

--- GoodGuysCommunity/GoodGuysCommunity.Web/Areas/Forum/Models/CommentsViewModel.cs
+++ GoodGuysCommunity/GoodGuysCommunity.Web/Areas/Forum/Models/CommentsViewModel.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace GoodGuysCommunity.Web.Areas.Forum.Models
+﻿namespace GoodGuysCommunity.Web.Areas.Forum.Models
 {
     public class CommentsViewModel
     {
         public int Id { get; set; }
 
         public string Content { get; set; }
-
     }
 }
